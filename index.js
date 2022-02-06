f477127606aea473262e915c3ce2bfae3c840f98
Anas Yahya <anasyahya@anass-imac.localdomain>
2022-02-06 14:30:16 +0100

    create the Level

--- index.js
+++ index.js
@@ -5,16 +5,47 @@ window.addEventListener("load", function () {
   const gameResult = document.querySelector("#game-result");
   const scoreNumber = document.querySelector("#score-number");
   const startGame = document.querySelector("#start-game");
+  const highScore = document.querySelector("#high-score-number");
+  const btnEasy = document.querySelector("#btn-easy");
+  const btnMedium = document.querySelector("#btn-medium");
+  const btnHard = document.querySelector("#btn-hard");
+
+  btnEasy.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  btnMedium.addEventListener("click", function () {
+    modal.style.display = "none";
+    score = 10;
+    level = 10;
+    scoreNumber.textContent = score;
+  });
+
+  btnHard.addEventListener("click", function () {
+    modal.style.display = "none";
+    score = 5;
+    level = 5;
+    scoreNumber.textContent = score;
+  });
 
   let random = getRandomNumber();
   //gameResult.textContent = random;
   let score = 20;
+  let level = 20;
+  let highScoreNumber = 0;
 
   checkBtn.addEventListener("click", function () {
     const guess = inputNumber.value;
-    if (guess == random) {
+    if (!guess) {
+      message.textContent = "No number !";
+    } else if (guess == random) {
       message.textContent = "Correct Number!";
       gameResult.textContent = random;
+
+      if (score > highScoreNumber) {
+        highScoreNumber = score;
+        highScore.textContent = highScoreNumber;
+      }
     } else if (guess > random) {
       message.textContent = "To High!";
       score--;
@@ -24,16 +55,28 @@ window.addEventListener("load", function () {
       score--;
       scoreNumber.textContent = score;
     }
+    if (score === 0) {
+      message.textContent = "You lost the Game !";
+    }
   });
   startGame.addEventListener("click", function () {
+    score = level;
     random = getRandomNumber();
     message.textContent = "Start Guessing...!";
     inputNumber.value = "";
-    scoreNumber.textContent = "20";
+    scoreNumber.textContent = score;
     gameResult.textContent = "?";
   });
 
   function getRandomNumber() {
     return Math.floor(Math.random() * 20) + 1;
   }
+
+  document.addEventListener("keypress", function (event) {
+    let keycode = event.keycode;
+    if (keycode > 48 && keycode < 57) {
+      return true;
+    }
+    return false;
+  });
 });
